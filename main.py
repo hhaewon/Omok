fa941e0e3fb6cb4ec435ae1f9da4ddb3bfe99e6f
ShinHaewon <96557453+hhaewon@users.noreply.github.com>
2023-10-14 11:08:32 +0900

    create evaluate function

--- main.py
+++ main.py
@@ -125,6 +125,129 @@ class Omok:
             pygame.time.delay(200)
         self.make_text(msg[stone], center_x, 30)
 
+    def evaluate(self):
+        dir_x = [-1, 1, -1, 1, 0, 0, 1, -1]
+        dir_y = [0, 0, -1, 1, -1, 1, -1, 1]
+        ai_weight = 0
+        player_weight = 0
+
+        for x in range(15):
+            for y in range(15):
+                if self.board[y][x] == Stone.EMPTY:
+                    continue
+
+                nx, ny = x, y
+                flag = False
+                is_one_space = False
+                is_one_side_block = False
+                cur_stone = self.board[y][x]
+
+                for k in range(0, 8, 2):
+                    if self.rule.is_fit_five(x, y, k):
+                        continue
+
+                    nx, ny = x, y
+                    stone_cnt = 1
+                    flag = False
+                    is_one_space = False
+
+                    is_one_side_block = False
+
+                    if k != 0 and (x == 0 or y == 0 or x == 14 or y == 14):
+                        is_one_side_block = True
+
+                    for i in range(4):
+                        nx += dir_x[k]
+                        ny += dir_y[k]
+
+                        if Rule.is_invalid(nx, ny):
+                            break
+
+                        if self.board[ny][nx] == Stone.EMPTY and is_one_space:
+                            break
+                        if self.board[ny][nx] == cur_stone:
+                            stone_cnt += 1
+                            if flag:
+                                is_one_space = True
+                                flag = False
+                            if nx == 0 or ny == 0 or nx == 14 or ny == 14:
+                                is_one_side_block = True
+                        elif self.board[ny][nx] == Stone.EMPTY and not flag:
+                            flag = True
+                        else:
+                            if not flag:
+                                is_one_side_block = True
+                            break
+
+                    nx, ny = x, y
+                    flag = False
+
+                    for i in range(4):
+                        nx += dir_x[k + 1]
+                        ny += dir_y[k + 1]
+
+                        if Rule.is_invalid(nx, ny):
+                            break
+
+                        if self.board[ny][nx] == Stone.EMPTY and is_one_space:
+                            break
+
+                        if self.board[ny][nx] == cur_stone:
+                            stone_cnt += 1
+                            if flag:
+                                is_one_space = True
+                                flag = False
+                            if nx == 0 or ny == 0 or nx == 14 or ny == 14:
+                                is_one_side_block = True
+                        elif self.board[ny][nx] == Stone.EMPTY and not flag:
+                            flag = True
+                        else:
+                            if not flag:
+                                is_one_side_block = True
+                            break
+
+                    weight_sum = 0
+
+                    if stone_cnt == 1:
+                        if is_one_side_block and not is_one_space:
+                            weight_sum += 5
+                        elif not is_one_side_block and not is_one_space:
+                            weight_sum += 10
+                    elif stone_cnt == 2:
+                        if is_one_side_block and not is_one_space:
+                            weight_sum += 30
+                        elif is_one_side_block and is_one_space:
+                            weight_sum += 15
+                        elif not is_one_side_block and not is_one_space:
+                            weight_sum += 40
+                        else:
+                            weight_sum += 30
+                    elif stone_cnt == 3:
+                        if is_one_side_block and not is_one_space:
+                            weight_sum += 60
+                        elif is_one_side_block and is_one_space:
+                            weight_sum += 120
+                        elif not is_one_side_block and not is_one_space:
+                            weight_sum += 400
+                        else:
+                            weight_sum += 360
+                    elif stone_cnt == 4:
+                        if is_one_side_block and not is_one_space:
+                            weight_sum += 200
+                        elif is_one_side_block and is_one_space:
+                            weight_sum += 190
+                        elif not is_one_side_block and not is_one_space:
+                            weight_sum += 1500
+                        else:
+                            weight_sum += 660
+                    elif stone_cnt == 5 or stone_cnt == 6:
+                        weight_sum += 2000
+
+                    if cur_stone == Stone.BLACK:
+                        player_weight += weight_sum
+                    else:
+                        ai_weight += weight_sum
+        return ai_weight - player_weight
 
 def main():
     pygame.init()
