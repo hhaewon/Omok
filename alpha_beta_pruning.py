b9df1723c0e859c32432315e42cb16718bc0db38
ShinHaewon <96557453+hhaewon@users.noreply.github.com>
2023-10-14 13:52:09 +0900

    edit alpha_beta_pruning.py

--- alpha_beta_pruning.py
+++ alpha_beta_pruning.py
@@ -1,81 +1,131 @@
+import math
+import random
 import multiprocessing
 
-
 class AlphaBetaPruning:
-    def __init__(self):
+    def __init__(self, depth, maximizing_player):
+        self.depth = depth
+        self.maximizing_player = maximizing_player
         self.best_move = None
 
     def alpha_beta_search(self, board):
         self.best_move = None
-        max_value = float('-inf')
-        legal_moves = self.generate_legal_moves(board)
+        if self.maximizing_player:
+            value = float('-inf')
+            legal_moves = board.generate_legal_moves()
 
-        if len(legal_moves) == 0:
-            return None
+            # 병렬 처리를 위한 프로세스 풀 생성
+            pool = multiprocessing.Pool()
 
-        # 병렬 처리를 위한 프로세스 풀 생성
-        pool = multiprocessing.Pool()
+            results = pool.map(self.min_value, [(board.make_copy(), move, self.depth, float('-inf'), float('inf')) for move in legal_moves])
 
-        # 병렬로 Alpha-Beta 탐색 수행
-        results = pool.map(self.min_value, [(board.make_move(move),) for move in legal_moves])
+            pool.close()
+            pool.join()
 
-        pool.close()
-        pool.join()
+            for move, result in zip(legal_moves, results):
+                if result > value:
+                    value = result
+                    self.best_move = move
+        else:
+            value = float('inf')
+            legal_moves = board.generate_legal_moves()
 
-        for move, value in zip(legal_moves, results):
-            if value > max_value:
-                max_value = value
-                self.best_move = move
+            # 병렬 처리를 위한 프로세스 풀 생성
+            pool = multiprocessing.Pool()
 
+            results = pool.map(self.max_value, [(board.make_copy(), move, self.depth, float('-inf'), float('inf')) for move in legal_moves])
+
+            pool.close()
+            pool.join()
+
+            for move, result in zip(legal_moves, results):
+                if result < value:
+                    value = result
+                    self.best_move = move
         return self.best_move
 
-    def max_value(self, board):
-        if self.cutoff_test(board):
+    def max_value(self, args):
+        board, move, depth, alpha, beta = args
+        if depth == 0 or board.is_game_over():
             return self.evaluate(board)
+        value = float('-inf')
+        board.make_move(move)
+        legal_moves = board.generate_legal_moves()
+        for next_move in legal_moves:
+            value = max(value, self.min_value((board.make_copy(), next_move, depth - 1, alpha, beta)))
+            alpha = max(alpha, value)
+            if beta <= alpha:
+                break
+        return value
 
-        v = float('-inf')
-        legal_moves = self.generate_legal_moves(board)
-        for move in legal_moves:
-            v = max(v, self.min_value(board.make_move(move)))
-        return v
-
-    def min_value(self, board):
-        if self.cutoff_test(board):
+    def min_value(self, args):
+        board, move, depth, alpha, beta = args
+        if depth == 0 or board.is_game_over():
             return self.evaluate(board)
-
-        v = float('inf')
-        legal_moves = self.generate_legal_moves(board)
-        for move in legal_moves:
-            v = min(v, self.max_value(board.make_move(move)))
-        return v
-
-    def cutoff_test(self, board):
-        # 게임 종료 조건을 정의 (예: 최대 탐색 깊이, 승패 판정)
-        pass
+        value = float('inf')
+        board.make_move(move)
+        legal_moves = board.generate_legal_moves()
+        for next_move in legal_moves:
+            value = min(value, self.max_value((board.make_copy(), next_move, depth - 1, alpha, beta)))
+            beta = min(beta, value)
+            if beta <= alpha:
+                break
+        return value
 
     def evaluate(self, board):
-        # 게임 상태를 가치화하는 휴리스틱 함수를 정의
+        # 게임 보드의 상태를 가치화하는 휴리스틱 함수를 정의
         pass
 
-    def generate_legal_moves(self, board):
-        # 가능한 움직임을 생성
-        pass
-
-
+# 게임보드 클래스
 class GameBoard:
-    def __init__(self):
-        # 게임 보드 초기화
-        pass
+    def __init__(self, size):
+        self.size = size
+        self.board = [[0] * size for _ in range(size)]
+        self.current_player = 1
 
     def make_move(self, move):
-        # 움직임 수행
-        pass
+        x, y = move
+        if self.is_valid_move(x, y):
+            self.board[y][x] = self.current_player
+            self.current_player = -self.current_player  # 플레이어 교체
+            return True
+        else:
+            return False
 
+    def is_valid_move(self, x, y):
+        if 0 <= x < self.size and 0 <= y < self.size and self.board[y][x] == 0:
+            return True
+        return False
+
+    def generate_legal_moves(self):
+        legal_moves = []
+        for x in range(self.size):
+            for y in range(self.size):
+                if self.is_valid_move(x, y):
+                    legal_moves.append((x, y))
+        random.shuffle(legal_moves)  # 가능한 움직임을 무작위로 섞음
+        return legal_moves
+
+    def make_copy(self):
+        copied_board = GameBoard(self.size)
+        copied_board.board = [row[:] for row in self.board]
+        copied_board.current_player = self.current_player
+        return copied_board
+
+    def is_game_over(self):
+        # 게임 종료 조건을 판단 (이 부분은 게임 규칙에 따라 수정해야 합니다.)
+        return False
 
 if __name__ == '__main__':
-    game_board = GameBoard()
-    alpha_beta = AlphaBetaPruning()
+    game_board = GameBoard(15)
+    depth = 4  # 탐색 깊이 설정
+    maximizing_player = True  # Maximizer 플레이어
+    alpha_beta = AlphaBetaPruning(depth, maximizing_player)
 
-    # Alpha-Beta 탐색을 사용하여 최선의 수 계산
-    best_move = alpha_beta.alpha_beta_search(game_board)
-    print("Best Move:", best_move)
+    while not game_board.is_game_over():
+        if game_board.current_player == 1:
+            best_move = alpha_beta.alpha_beta_search(game_board)
+            game_board.make_move(best_move)
+            print("Maximizer's Move:", best_move)
+        else:
+            pass
